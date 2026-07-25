58577561eb8935cee1384ba54b984dd9bef9b4cb
Tarun Pathuri <ptarunul@gmail.com>
2026-07-25 14:02:36 +0100

    Add: search-2d-matrix - submission-8

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-8.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n=matrix.length;
+        int m= matrix[0].length;
+        int top=0;
+        int bottom=n-1;
+
+        while(top<=bottom){
+            int midrow=top+(bottom-top)/2;
+
+            if(target>matrix[midrow][m-1]){
+                top=midrow+1;
+            }else if(target<matrix[midrow][0]){
+                bottom=midrow-1;
+            }else{
+                break;
+            }
+        }
+
+        if(!(top<=bottom)){
+            return false;
+        }
+
+        int row= top+(bottom-top)/2;
+        int l=0,r=m-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(target>matrix[row][mid]){
+                l=mid+1;
+            }else if(target<matrix[row][mid]){
+                r=mid-1;
+            }else{
+                return true;
+            }
+        }
+
+    return false;
+
+
+    }
+}
