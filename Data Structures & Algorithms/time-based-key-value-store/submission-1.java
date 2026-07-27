08bec5af1f1d77361597ab709bc0f55ae8c5a5ad
Tarun Pathuri <ptarunul@gmail.com>
2026-07-27 11:35:11 +0100

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.java
@@ -0,0 +1,66 @@
+// class TimeMap {
+//     class Pair{
+//         int timeStamp;
+//         String value;
+
+//         Pair(int timeStamp, String value){
+//             this.timeStamp= timeStamp;
+//             this.value=value;
+//         }
+//     }
+
+//     Map<String, List<Pair>> map;
+    
+//     public TimeMap() {
+//         map= new HashMap<>();
+//     }
+
+//     public void set(String key, String value, int timestamp) {
+//         map.putIfAbsent(key, new ArrayList<>());
+//         map.get(key).add(new Pair(timestamp, value));
+//     }
+
+//     public String get(String key, int timestamp) {
+//         if(!map.containsKey(key)) return "";
+
+//         List<Pair> list= map.get(key);
+
+//         int l=0, r=list.size()-1;
+//         String res= "";
+//         while(l<=r){
+//             int mid= l+(r-l)/2;
+
+//             if(list.get(mid).timeStamp<=timestamp){
+//                 res= list.get(mid).value;
+//                 l= mid+1;
+//             }else{
+//                 r= mid-1;
+//             }
+//         }
+//         return res;
+
+//     }
+// }
+class TimeMap {
+    Map<String, String> m = new HashMap<>();
+
+    public TimeMap() {
+        
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        String k = timestamp + key;
+        m.put(k, value);
+    }
+    
+    public String get(String key, int timestamp) {
+        
+        for (int i = timestamp ;i>0;i--)
+        {
+            String k = i + key;
+            if(m.get(k)!=null){return m.get(k);}
+        }
+        return "";
+        
+    }
+}
