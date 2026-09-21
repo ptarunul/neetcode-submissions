6aebbe57b3b93380f6b106d5c9be77200cfde093
Tarun Pathuri <ptarunul@gmail.com>
2026-09-21 11:08:34 +0100

    Add: is-anagram - submission-19

---
+++ Data Structures & Algorithms/is-anagram/submission-19.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] arr= new int[26];
+        int n= s.length();
+        int m= t.length();
+
+        if(n!=m){
+            return false;
+        }
+
+        for(int i=0;i<n;i++){
+            arr[s.charAt(i)-'a']++;
+            arr[t.charAt(i)-'a']--;
+        }
+
+
+        for(int i:arr){
+            if(i!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
